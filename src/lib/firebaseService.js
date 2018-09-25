fb69b66871eaaf45187679b981fefd50e89c7afa
Fredrik Ringlander <dev.ringlander@gmail.com>
2018-09-25 15:25:15 +0200

    Firebase service handling all interactions with firebase.

--- src/lib/firebaseService.js
+++ src/lib/firebaseService.js
@@ -1,41 +1,194 @@
 import db from "@/firebaseInit";
+import { ok } from "assert";
 
-class FirebaseService{
-  constructor(){
-
+class FirebaseService {
+  constructor() {
+    const settings = {
+      timestampsInSnapshots: true
+    };
+    db.settings(settings);
   }
+  async getTracksForPlaylist(playlistId) {
+    let tracks = [];
 
-  async getUserByEmail(userEmail){
+    const trackQueryResponse = await db
+      .collection(`playlists/${playlistId}/tracks`)
+      .get();
+
+    trackQueryResponse.docs.forEach(doc => {
+      console.log(doc.data());
+      let trackObj = {
+        id: doc.id,
+        spotifyTrackID: doc.data().spotifyTrackId,
+        spotifyAddedAt: doc.data().spotifyAddedAt,
+        name: doc.data().name,
+        album: doc.data().album,
+        artist: doc.data().artist,
+        tempo: doc.data().tempo,
+        key: doc.data().key,
+        timeSignature: doc.data().timeSignature,
+        duration: doc.data().duration
+      };
+      tracks.push(trackObj);
+    });
+    return tracks;
+  }
+  async getPlaylistsByUser(userId) {
+    let pls = [];
+    const queryResponse = await db
+      .collection("playlists")
+      .where("owner", "==", userId)
+      .get();
+    queryResponse.docs.forEach(doc => {
+      let plObj = {
+        id: doc.id,
+        name: doc.data().name,
+        url: doc.data().href,
+        image: doc.data().image,
+        owner: doc.data().owner,
+        playlistHash: doc.data().playlistHash,
+        spotifyId: doc.data().spotifyId,
+        spotifyUser: doc.data().spotifyUser,
+        updatedAt: doc.data().updatedAt,
+        needSync: false,
+        tracks: new Array(),
+        source: "FIREBASE"
+      };
+      pls.push(plObj);
+    });
+    pls.forEach(async pl => {
+      const trackQueryResponse = await db
+        .collection(`playlists/${pl.id}/tracks`)
+        .get();
+      trackQueryResponse.docs.forEach(doc => {
+        console.log(doc.data());
+        let trackObj = {
+          id: doc.id,
+          spotifyTrackID: doc.data().spotifyTrackId,
+          spotifyAddedAt: doc.data().spotifyAddedAt,
+          name: doc.data().name,
+          album: doc.data().album,
+          artist: doc.data().artist,
+          tempo: doc.data().tempo,
+          key: doc.data().key,
+          timeSignature: doc.data().timeSignature,
+          duration: doc.data().duration
+        };
+        pl.tracks.push(trackObj);
+      });
+    });
+    return pls;
+  }
+  /**
+   *
+   * @param {*} userEmail
+   */
+  async getUserByEmail(userEmail) {
     let userObj = null;
-    const queryResponse = await db.collection('users').where("userEmail","==",userEmail).get();
-    if(queryResponse.docs.length===1){
+    const queryResponse = await db
+      .collection("users")
+      .where("userEmail", "==", userEmail)
+      .get();
+    if (queryResponse.docs.length === 1) {
       userObj = {
         userName: queryResponse.docs[0].data().userName,
         authProvider: queryResponse.docs[0].data().authProvider,
-        spotifyUserId: queryResponse.docs[0].data().spotifyUserId,
+        spotifySettings: queryResponse.docs[0].data().spotifySettings,
         userId: queryResponse.docs[0].data().userId,
         lastLoginAt: queryResponse.docs[0].data().lastLoginAt,
-        autoConnectToSpotify: queryResponse.docs[0].data().autoConnectToSpotify
-      }
+        userImage: queryResponse.docs[0].data().userImage,
+        fullName: queryResponse.docs[0].data().fullName,
+        name: queryResponse.docs[0].data().name
+      };
     }
     return userObj;
   }
+  /**
+   *
+   * @param {*} updateUser
+   */
+  async updateUserProfile(updateUser) {
+    let obj = null;
+    const userQueryResponse = await db
+      .collection("users")
+      .where("userId", "==", updateUser.userId)
+      .get();
+    if (userQueryResponse.docs.length === 1) {
+      userQueryResponse.docs.forEach(doc => {
+        doc.ref
+          .update({
+            userName: updateUser.userName,
+            name: updateUser.name,
+            spotifySettings: updateUser.spotifySettings,
+            fullName: updateUser.fullName
+          })
+          .then(() => {
+            obj = Date.now();
+          })
+          .catch(err => {
+            return err;
+          });
+      });
+    }
+    return obj;
+  }
+  /**
+   *
+   * @param {*} editUser
+   */
+  updateUser(editUser) {
+    return new Promise((resolve, reject) => {
+      let returnObj = {
+        status: "",
+        message: ""
+      };
+      db.collection("users")
+        .where("userId", "==", editUser.userId)
+        .get()
+        .then(qs => {
+          qs.forEach(doc => {
+            doc.ref
+              .update({
+                userName: editUser.userName,
+                name: editUser.name,
+                spotifySettings: editUser.spotifySettings,
+                updatedAt: new Date(),
+                fullName: editUser.fullName
+              })
+              .then(() => {
+                returnObj = {
+                  status: "10",
+                  message: "User updated successfully"
+                };
 
-  async getUserPlaylists(userId){
-    let pls=[];
-    const userPlaylists = await db.collection('playlists').where("userId","==",userId).get();
-    userPlaylists.docs.forEach(doc=>{
+                resolve(returnObj);
+              })
+              .catch(err => {
+                reject(Error(err));
+              });
+          });
+        });
+    });
+  }
+
+  async getUserPlaylists(userId) {
+    let pls = [];
+    const userPlaylists = await db
+      .collection("playlists")
+      .where("userId", "==", userId)
+      .get();
+    userPlaylists.docs.forEach(doc => {
       let plObj = {
         id: doc.id,
         url: doc.data().href,
         image: doc.data().image,
         owner: doc.data().owner,
         playlistHash: doc.data().playlistHash,
-        spotifyId:doc.data().spotifyId,
+        spotifyId: doc.data().spotifyId,
         spotifyUser: doc.data().spotifyUser,
         updatedAt: doc.data().updatedAt
         /**
-         * 
+         *
          * href
          * image :{
          * }
@@ -46,9 +199,9 @@ class FirebaseService{
          * }
          * updatedAt
          * tracks: []
-         * 
+         *
          */
-      }
+      };
       pls.push(plObj);
     });
     return pls;
--- src/store/modules/session.js
+++ src/store/modules/session.js
@@ -1,98 +1,152 @@
-import Authenticator from '@/lib/authenticator';
-import {USER_BY_EMAIL_REQUEST} from '../actions/user';
-import { STATUS_CODES } from 'http';
-import { stat } from 'fs';
+import Authenticator from "@/lib/authenticator";
+import {
+  USER_BY_EMAIL_REQUEST,
+  USER_GET_SPOTIFY_USER_REQUEST
+} from "../actions/user";
+import { STATUS_CODES } from "http";
+import { stat } from "fs";
 
 const auth = new Authenticator();
 
 const state = {
-  authenticated: !!localStorage.getItem('auth_access_token'),
-  accessToken: localStorage.getItem('auth_access_token'),
-  idToken: localStorage.getItem('auth_id_token'),
-  expiresAt: localStorage.getItem('auth_expires_at'),
-  user: JSON.parse(localStorage.getItem('auth_user'))
+  authenticated: !!localStorage.getItem("auth_access_token"),
+  accessToken: localStorage.getItem("auth_access_token"),
+  idToken: localStorage.getItem("auth_id_token"),
+  expiresAt: localStorage.getItem("auth_expires_at"),
+  user: JSON.parse(localStorage.getItem("auth_user")),
+  authenticatedWithSpotify: !!localStorage.getItem("spotify_access_token"),
+  spotifyAccessToken: localStorage.getItem("spotify_access_token"),
+  spotifyRefreshToken: localStorage.getItem("spotify_refresh_token"),
+  spotifyTokenExpiresAt: localStorage.getItem("spotify_token_expires_at")
 };
 
 const getters = {
-  AUTHENTICATED: state=>{
+  AUTHENTICATED: state => {
     return state.authenticated;
   },
-  AUTH_USER: state=>{
+  AUTH_USER: state => {
     return state.user;
   },
-  IS_AUTHENTICATED: state=>{
-
+  SPOTIFY_ACCESS_TOKEN: state => {
+    return state.spotifyAccessToken;
+  },
+  AUTHENTICATED_WITH_SPOTIFY: state => {
+    return state.authenticatedWithSpotify;
+  },
+  SPOTIFY_TOKEN_HAS_EXPIRED: state => {
+    return state.spotifyTokenExpiresAt < Date.now() / 1000;
+  },
+  SPOTIFY_TOKEN_EXPIRES_AT: state => {
+    return state.spotifyTokenExpiresAt;
   }
-
 };
 
 const mutations = {
-  SET_AUTHENTICATED: (state, authData)=>{
-    state.authenticated = true,
-    state.accessToken = authData.accessToken,
-    state.idToken = authData.idToken,
-    state.expiresAt = authData.expiresIn * 1000 + new Date().getTime(),
-    state.user = authData.idTokenPayload
+  SET_AUTHENTICATED: (state, authData) => {
+    (state.authenticated = true),
+      (state.accessToken = authData.accessToken),
+      (state.idToken = authData.idToken),
+      (state.expiresAt = authData.expiresIn * 1000 + new Date().getTime()),
+      (state.user = authData.idTokenPayload);
 
-    localStorage.setItem('auth_access_token',state.accessToken);
-    localStorage.setItem('auth_id_token',state.idToken);
-    localStorage.setItem('auth_expires_at',state.expiresAt);
-    localStorage.setItem('auth_user', JSON.stringify(state.user));
-
-    
+    localStorage.setItem("auth_access_token", state.accessToken);
+    localStorage.setItem("auth_id_token", state.idToken);
+    localStorage.setItem("auth_expires_at", state.expiresAt);
+    localStorage.setItem("auth_user", JSON.stringify(state.user));
   },
-  SET_AUTH_USER: (state, userData)=>{
+  SET_SPOTIFY_AUTHENTICATED: (state, authData) => {
+    (state.authenticatedWithSpotify = true),
+      (state.spotifyAccessToken = authData.access_token),
+      (state.spotifyRefreshToken = authData.refresh_token),
+      (state.spotifyTokenExpiresAt =
+        authData.expires_in * 1000 + new Date().getTime());
+
+    localStorage.setItem("spotify_access_token", state.spotifyAccessToken);
+    localStorage.setItem("spotify_refresh_token", state.spotifyRefreshToken);
+    localStorage.setItem(
+      "spotify_token_expires_at",
+      state.spotifyTokenExpiresAt
+    );
+  },
+  SET_NOT_SPOTIFY_AUTHENTICATED: state => {
+    (state.spotifyAccessToken = null),
+      (state.authenticatedWithSpotify = false),
+      (state.spotifyTokenExpiresAt = null),
+      (state.spotifyRefreshToken = null);
+
+    localStorage.removeItem("spotify_access_token");
+    localStorage.removeItem("spotify_token_expires_at");
+    localStorage.removeItem("spotify_refresh_token");
+  },
+
+  SET_AUTH_USER: (state, userData) => {
     state.user = userData;
   },
-  SET_NOT_AUTHENTICATED: (state)=>{
-    state.authenticated = false,
-    state.accessToken = null,
-    state.idToken = false,
-    state.user = null
+  SET_NOT_AUTHENTICATED: state => {
+    (state.authenticated = false),
+      (state.accessToken = null),
+      (state.idToken = false),
+      (state.user = null);
 
-    localStorage.removeItem('auth_access_token');
-    localStorage.removeItem('auth_id_token');
-    localStorage.removeItem('auth_expires_at');
-    localStorage.removeItem('auth_user');
-  
+    localStorage.removeItem("auth_access_token");
+    localStorage.removeItem("auth_id_token");
+    localStorage.removeItem("auth_expires_at");
+    localStorage.removeItem("auth_user");
   }
 };
 
 const actions = {
-  login(){
-    auth.login()
+  login() {
+    auth.login();
   },
-  LOGOUT({commit}){
-    commit('SET_NOT_AUTHENTICATED')
+  LOGOUT({ commit }) {
+    commit("SET_NOT_AUTHENTICATED");
   },
-  HANDLE_AUTH: ({commit,dispatch})=>{
-    return new Promise((resolve, reject)=>{
-      auth.handleAuthentication()
-        .then((authResult)=>{
-          commit('SET_AUTHENTICATED',authResult);
-          if(state.user.email){
-            dispatch(USER_BY_EMAIL_REQUEST,state.user.email)
-            .then(resp=>{
-              if(resp!=null){
-                resolve(resp);
-              }
-              else{
-                reject(Error("No user profile found in Firestore"));
-              }
-            })
-            .catch(err=>{
-              reject(err);
-            });
-          } 
+  LOGOUT_FROM_SPOTIFY({ commit }) {
+    commit("SET_NOT_SPOTIFY_AUTHENTICATED");
+  },
+
+  HANDLE_SPOTIFY_AUTH: ({ commit, dispatch }, authData) => {
+    return new Promise((resolve, reject) => {
+      commit("SET_SPOTIFY_AUTHENTICATED", authData);
+      dispatch(USER_GET_SPOTIFY_USER_REQUEST, state.spotifyAccessToken)
+        .then(resp => {
+          resolve(resp);
         })
-        .catch(err=>{console.log('This is the err: '+err)});
+        .catch(err => {
+          reject(err);
+        });
+    });
+  },
+  HANDLE_AUTH: ({ commit, dispatch }) => {
+    return new Promise((resolve, reject) => {
+      auth
+        .handleAuthentication()
+        .then(authResult => {
+          commit("SET_AUTHENTICATED", authResult);
+          if (state.user.email) {
+            dispatch(USER_BY_EMAIL_REQUEST, state.user.email)
+              .then(resp => {
+                if (resp != null) {
+                  resolve(resp);
+                } else {
+                  reject(Error("No user profile found in Firestore"));
+                }
+              })
+              .catch(err => {
+                reject(err);
+              });
+          }
+        })
+        .catch(err => {
+          console.log("This is the err: " + err);
+        });
     });
   }
-
-}
-export default{
+};
+export default {
   state,
   getters,
   mutations,
   actions
-}
+};
