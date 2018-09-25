57437e9c435b04dbb3ad913013bae1cac58b2c05
Fredrik Ringlander <dev.ringlander@gmail.com>
2018-09-25 15:31:48 +0200

    Added constants for userURL and audioURL. Removed redundant code

--- src/spotifyConfig.js
+++ src/spotifyConfig.js
@@ -1,22 +1,20 @@
 export default {
-  //baseAPI: "https://api.spotify.com/v1",
   userEndpoint: "/me",
   currentUserPlaylistsEndpoint: "/playlists",
-  
+  userURL: "https://api.spotify.com/v1/me",
   userPlaylistsURL: "https://api.spotify.com/v1/me",
   userMyPlaylistsURL: "https://api.spotify.com/v1/me/playlists?limit=50",
-  //userURL: baseAPI+userEndPoint,
+  audioURL: "https://api.spotify.com/v1/audio-features?ids=",
 
-  playlistTracksEndpoint: function(user_id, playlist_id){
+  playlistTracksEndpoint: function(user_id, playlist_id) {
     return `https://api.spotify.com/v1/users/${user_id}/playlists/${playlist_id}/tracks`;
   },
-  spotifyHeaders: function(access_token){
+  spotifyHeaders: function(access_token) {
     return {
-      "Authorization":"Bearer " + access_token
+      Authorization: "Bearer " + access_token
     };
   }
-  
+
   //	https://api.spotify.com/v1/users/{user_id}/playlists/{playlist_id}/tracks
   //  https://api.spotify.com/v1/me/playlists
-
-}
+};
