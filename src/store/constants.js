22d7c60a5f7339d826e950060f6f9b4a5c57a284
Fredrik Ringlander <dev.ringlander@gmail.com>
2018-09-28 22:34:01 +0200

    Added constants for the different playlist statuses.
    Each constant is represented as an object with  a status and message

---
+++ src/store/constants.js
@@ -0,0 +1,56 @@
+export const SPOTIFY_LOADING_PLAYLISTS = {
+  status: 101,
+  message: "Fetching Spotify playlists...."
+};
+export const SPOTIFY_LOADING_PLAYLISTS_DONE = {
+  status: 201,
+  message: "Done fetching Spotify playlists"
+};
+export const SPOTIFY_LOADING_PLAYLISTS_TRACKS = {
+  status: 111,
+  message: "Fetching and processing tracks for Spotify playlists"
+};
+export const SPOTIFY_LOADING_PLAYLISTS_TRACKS_DONE = {
+  status: 211,
+  message: "Done fetching and processing tracks for Spotify playlists"
+};
+export const SPOTIFY_LOADING_AUDIO_FEATURES = {
+  status: 131,
+  message: "Fetching audio features for Spotify playlist tracks"
+};
+export const SPOTIFY_LOADING_AUDIO_FEATURES_DONE = {
+  status: 231,
+  message: "Done fetching audio features for Spotify playlist tracks"
+};
+export const SPOTIFY_MERGING_TRACKS_AUDIO_FEATURES = {
+  status: 141,
+  message: "Merging audio features with Spotify playlist tracks"
+};
+export const SPOTIFY_MERGING_TRACKS_AUDIO_FEATURES_DONE = {
+  status: 241,
+  message: "Done merging audio features with Spotify playlist tracks"
+};
+export const FIRESTORE_LOADING_PLAYLISTS = {
+  status: 301,
+  message: "Fetching playlists from Firestore"
+};
+export const FIRESTORE_LOADING_PLAYLISTS_DONE = {
+  status: 401,
+  message: "Done fetching playlists from Firestore"
+};
+export const FIRESTORE_LOADING_PLAYLISTS_TRACKS = {
+  status: 311,
+  message: "Fetching tracks for Firestore playlists"
+};
+export const FIRESTORE_ADDING_TRACKS_TO_PLAYLIST = {
+  status: 321,
+  message: "Adding tracks to Firestore playlists"
+};
+export const FIRESTORE_LOADING_PLAYLISTS_TRACKS_DONE = {
+  status: 411,
+  message: "Done processing tracks for Firestore playlists"
+};
+/* export const FIRESTORE_LOADING_PLAYLISTS_DONE = {
+  status: 311,
+  message: "Done fetching playlists from Firestore"
+}; */
